3c4ce23c54e39fbaf23f321fe3ddbec323e08186
Rickard Armiento <gitcommits@armiento.net>
2022-09-24 23:48:01 +0200

    Better cleaning, separate comics, RPG books

--- 2-clean-library.sh
+++ 2-clean-library.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ ! -e ./HumbleBundleLibrary ]; then
     echo "Please make a symbolic link named 'HumbleBundleLibrary' that points to where you want the Humble Bundle Library"
     echo "e.g., ln -s /path/where/library/is ./HumbleBundleLibrary"
@@ -7,14 +9,37 @@ if [ ! -e ./HumbleBundleLibrary ]; then
     exit 1
 fi
 
-mkdir -p rmlint
-cd rmlint
-
-echo "Look for empty dirs - investigate manually"
-rmlint --types "emptyfiles,emptydirs"  -o pretty:stdout ../HumbleBundleLibrary
-
-echo "Look for duplicated dirs"
-rmlint --types "dd,df" --merge-directories --honour-dir-layout -o progressbar -o sh:rmlint-dd.sh -c sh:symlink -o pretty:stdout -o summary:stdout -o json:rmlint-dd.json ../HumbleBundleLibrary
-
-echo "A script 'rmlint-dd.sh' has been created to remove and symlink duplicate directories."
-echo "Inspect it and run it."
+cd ./HumbleBundleLibrary
+mkdir -p PRODUCTS
+find . -mindepth 2 -maxdepth 2 -type d -not -path "./PRODUCTS/*" | while read DIR; do
+    echo "==== EXAMINING: $DIR"
+    PRODUCT="$(basename "$DIR")"
+    if [ -L "$DIR" ]; then
+	echo "$DIR is already a symbolic link, leaving alone"
+    elif [ -e "PRODUCTS/$PRODUCT" ]; then
+	echo "== $PRODUCT already exists"
+	if [ -n "$(ls -A "$DIR")" ]; then
+	    for FILE in "$DIR"/*; do
+		NAME="$(basename "$FILE")"
+		if [ -e "PRODUCTS/$PRODUCT/$NAME" ]; then
+		    echo "Same?" "$FILE" "PRODUCTS/$PRODUCT/$NAME"
+		    if ! cmp --silent "$FILE" "PRODUCTS/$PRODUCT/$NAME"; then
+			echo "SAME NAMED FILES ARE DIFFERENT?! ABORT!"
+			exit 1
+		    fi
+		    echo "Product directory has duplicate files, overwriting"
+		fi
+		mv "$FILE" "PRODUCTS/$PRODUCT/$NAME"
+	    done
+	else
+	    echo "Directory is empty, just replacing by symlink."
+	fi
+	rmdir "$DIR"
+	ln -s "../PRODUCTS/$PRODUCT" "$DIR"
+    else
+	echo "== $PRODUCT does not already exist"
+	mv "$DIR" "PRODUCTS/$PRODUCT"
+	ln -s "../PRODUCTS/$PRODUCT" "$DIR"
+	echo "Linked $DIR -> ../PRODUCTS/$PRODUCT"
+    fi
+done
--- 3-humble-bundle-library-to-calibre.sh
+++ 3-humble-bundle-library-to-calibre.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#set -e
+
 if [ ! -e ./HumbleBundleLibrary ]; then
     echo "Please make a symbolic link named 'HumbleBundleLibrary' that points to where you want the Humble Bundle Library"
     echo "e.g., ln -s /path/where/library/is ./HumbleBundleLibrary"
@@ -7,8 +9,9 @@ if [ ! -e ./HumbleBundleLibrary ]; then
     exit 1
 fi
 
-if [ ! -e ./CalibreLibrary ]; then
-    echo "Please make a symbolic link named 'CalibreLibrary' that points to where you want the Calibre Library"
+if [ ! -e ./CalibreLibrary -o ! -e ./CalibreLibraryComics -o ! -e ./CalibreLibraryRPG ]; then
+    echo "Please make symbolic links named 'CalibreLibrary', 'CalibreLibraryComics', 'CalibreLibraryRPG' for your Calibre Libraries."
+    echo "(The links can point to the same folder if you want all these in the same library)"
     echo "e.g., ln -s /path/where/library/is ./CalibreLibrary"
     echo
     exit 1
@@ -21,52 +24,119 @@ else
 fi
 
 STOP=0
-trap "STOP=1" SIGINT
+#trap "STOP=1" SIGINT
 
 MANUAL="$1"
 shift 1
 
+if $CALIBREDB --with-library ./CalibreLibrary/ add_custom_column source "Source" "text" 2>/dev/null; then
+    echo "Source custom column added to CalibreLibrary"
+fi
+if $CALIBREDB --with-library ./CalibreLibraryComics/ add_custom_column source "Source" "text" 2>/dev/null; then
+    echo "Source custom column added to CalibreLibraryComics"    
+fi
+if $CALIBREDB --with-library ./CalibreLibraryRPG/ add_custom_column source "Source" "text" 2>/dev/null; then
+    echo "Source custom column added to CalibreLibraryRPG"    
+fi
+
 (
     cd ./HumbleBundleLibrary
-    trap '' SIGINT    
+    #trap '' SIGINT    
     if [ -z "$MANUAL" ]; then
-	find . -maxdepth 3 -mindepth 3 -type f \( -name "*.pdf" -or -name "*.epub" -or -name "*.mobi" -or -name "*.cbz" -or -name "*.azw" -or -name "*.pdb" -or -name "*.prc" \) -print0 | xargs -0 -n 1 dirname | sort | uniq
+	find -L . -maxdepth 3 -mindepth 3 -path "./PRODUCTS/*" -prune -o -type f \( -name "*.pdf" -or -name "*.epub" -or -name "*.mobi" -or -name "*.cbz" -or -name "*.azw" -or -name "*.pdb" -or -name "*.prc" \) -print0 | xargs -0 -n 1 dirname | sort | uniq
     else
 	echo "$MANUAL"
     fi
 ) | (
-    trap "STOP=1" SIGINT
-    while read DIR; do
+  #trap "STOP=1" SIGINT
+  while read DIR; do
+
+    PRODUCTNAME=$(basename "$DIR" | tr -s " ")
+    PARENT=$(dirname "$DIR")
+    BUNDLENAME=$(basename "$PARENT" | tr -s " ")
+    if [ -L "./HumbleBundleLibrary/$DIR" ]; then
+	DIR=$(readlink "./HumbleBundleLibrary/$DIR")
+	DIR=${DIR#../}
+    fi
+
+    LIBRARY="./CalibreLibrary"
+    if grep -q "Comic" <<< "$BUNDLENAME"; then
+	LIBRARY="./CalibreLibraryComics"
+    elif grep -q "RPG" <<< "$BUNDLENAME"; then
+	LIBRARY="./CalibreLibraryRPG"
+    fi
+    LIBRARY="./CalibreLibrary"    
+    
+    echo "== BUNDLE: $BUNDLENAME | PRODUCT: $PRODUCTNAME | LIB: $LIBRARY"
+    
     if grep -q "^$DIR\$" ./humble_bundle_to_calibre_done.dat; then
-	#echo "Already done: $DIR"
+	echo "Already done"
 	continue
     fi
-    BASENAME=$(basename "$DIR" | tr -s " ")
-    NBR_GOOD=$(ls ./HumbleBundleLibrary/"$DIR"/*.{epub,mobi,cbz,azw,pdb,prc} 2>/dev/null | wc -l)
-    if [ "$NBR_GOOD" -ge "1" ]; then
-	echo "Adding book: $DIR"
-	$CALIBREDB --with-library ./CalibreLibrary/ --ignore "*.zip" --one-book-per-directory add ./HumbleBundleLibrary/"$DIR" "$@" &
-	WAIT=$!
-    else
-	echo "Adding book: $DIR with title $BASENAME"
-	$CALIBREDB --with-library ./CalibreLibrary/ add --ignore "*.zip" --title "$BASENAME" --one-book-per-directory ./HumbleBundleLibrary/"$DIR" "$@" &
-	WAIT=$!
-    fi
-    wait "$WAIT"
+
+    #NBR_GOOD=$(ls ./HumbleBundleLibrary/"$DIR"/*.{epub,mobi,cbz,azw,pdb,prc} 2>/dev/null | wc -l)
+
+    OUTPUT=$($CALIBREDB --with-library "$LIBRARY" add --ignore "*.zip" --title "$BUNDLENAME" --one-book-per-directory --tags "hb:${PRODUCTNAME// /_}" ./HumbleBundleLibrary/"$DIR" "$@" 2>&1)
     RETURN="$?"
-    if [ "$RETURN" == "0" ]; then
-	echo "$DIR" >> ./humble_bundle_to_calibre_done.dat
-    elif [ "$STOP" == "1" ]; then
-	wait "$WAIT"
-	RETURN="$?"
-	if [ "$RETURN" == "0" ]; then
-	    echo "$DIR" >> ./humble_bundle_to_calibre_done.dat
-	else
-	    echo "$DIR|$RETURN" >> ./humble_bundle_to_calibre_err.dat
-	fi
-	exit 0
+    echo "$OUTPUT"
+    if grep -q "already exist in the database" <<< "$OUTPUT"; then
+	continue
+    fi
+   
+    ID=$(echo "$OUTPUT" | sed 's/Added book ids: \(.*\)/\1/')
+    echo "Got ID: $ID"
+    if [ -z "$ID" ]; then
+	echo "Failed to extract id, perhaps something went wrong?"
+	continue
     else
-	echo "$DIR|$RETURN" >> ./humble_bundle_to_calibre_err.dat
+
+	META=$($CALIBREDB --with-library "$LIBRARY" show_metadata "$ID")
+	echo "Current metadata:"
+	printf "%s\n\n" "$META"
+	RETURN2="$?"
+	#COMMENTS=$(echo "$META" | awk '/^Comments *:/ {DUMP=1} DUMP==1 {print}')
+	#COMMENTS="${COMMENTS#*:}"
+	IDENTIFIERS=$(echo "$META" | grep '^Identifiers *:')
+	IDENTIFIERS="${IDENTIFIERS#*:}"
+	TITLE=$(echo "$META" | grep '^Title *:')
+	TITLE="${TITLE#*:}"
+	TAGS=$(echo "$META" | awk -F": " '/^Tags/ {print $2}')
+	if [ -n "$IDENTIFIERS" ]; then
+	    IDENTIFIERS_PREFIX="$IDENTIFIERS,"
+	else
+	    IDENTIFIERS_PREFIX=""
+
+	    IDENTIFIERS=
+	fi
+	#if [ -n "COMMENTS" ]; then
+	#    COMMENTS="$COMMENTS
+#
+#Source: Humble Bundle - $BUNDLENAME / ${PRODUCTNAME}
+#"
+#	else
+#	    COMMENTS="Source: Humble Bundle - $BUNDLENAME / ${PRODUCTNAME}"
+#	fi
+
+	# Keep the longest title
+	if [ "${#TITLE}" -lt "${#PRODUCTNAME}" ]; then
+	    TITLE="${PRODUCTNAME}"
+	fi
+	
+	#if [ "$NBR_GOOD" == "0" ]; then
+	#    TITLE="${PRODUCTNAME}"
+	#fi
+
+	if [ -n "$TAGS" ]; then
+	    TAGPREFIX="$(echo "$TAGS" | sed 's% / %,%g;'),"
+	else
+	    TAGPREFIX=""
+	fi
+	echo "Updated metadata:"
+	$CALIBREDB --with-library "$LIBRARY" set_metadata "$ID" -f "identifiers:${IDENTIFIERS_PREFIX}hb:${PRODUCTNAME// /_}" -f "#source:Humble Bundle: ${BUNDLENAME} / ${PRODUCTNAME}" -f "title:$TITLE" -f "tags:$TAGPREFIX,Humble Bundle"
+	RETURN3="$?"
+    fi
+    if [ "$RETURN" == "0" -a "$RETURN2" == "0" -a "$RETURN3" == "0" ]; then
+	echo "$DIR" >> ./humble_bundle_to_calibre_done.dat
     fi
     done
 )
